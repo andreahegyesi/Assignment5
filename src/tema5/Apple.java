3d3939625fee2141cf0b6bfe5a64ac4faaec7ccc
andreahegyesi <andrea.hegyesi1@gmail.com>
2023-03-15 15:59:20 +0200

    Added the *.java files

--- src/tema5/Apple.java
+++ src/tema5/Apple.java
@@ -2,7 +2,7 @@ package tema5;
 /**
  * Apple is a phone brand. Can't be instantiated.
  */
-public class Apple extends Phone {
+public abstract class Apple extends Phone {
     public Apple(int batteryLife, String color, String material, String IMEI) {
         super(batteryLife, color, material, IMEI);
     }
